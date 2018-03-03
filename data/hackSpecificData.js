a88fac44e842c94b654cf92e3bb46ea315ebe029
Justine Cocchi <justine@cocchi.org>
2018-03-02 19:01:34 -0800

    update hack data for hacktech

--- data/hackSpecificData.js
+++ data/hackSpecificData.js
@@ -1,38 +1,38 @@
 module.exports = {
-  hackName: 'Athena Hacks',
-  surveyLink: 'https://aka.ms/athenahack18',
+  hackName: 'HackTech',
+  surveyLink: 'https://aka.ms/hacktech',
   teamEmail: 'find us on slack!',
   prize: 'a GoPro Hero 6',
-  boothLocation: 'in the basement of Leavey Library',
+  boothLocation: 'in Chandler Cafe',
   teamMembers: [
+    {
+      name: 'Adam',
+      techFocus: ['.NET/ Core', 'Web Dev', 'Gaming'],
+      photoLink: ''
+    },
     {
       name: 'Annie',
       techFocus: ['Machine Learning', 'Logistics'],
       photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Annie.png'
     },
     {
-      name: 'Brandon',
-      techFocus: ['Mobile Development', 'Azure Functions', 'Cognitive Services'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Brandon.jpg'
+      name: 'Bret',
+      techFocus: ['Azure', 'IoT'],
+      photoLink: ''
     },
     {
-      name: 'James',
-      techFocus: ['Azure', 'Blockchain', 'JavaScript', 'Machine Learning'],
+      name: 'Daniel',
+      techFocus: ['Azure', 'Bot Framework'],
       photoLink: ''
     },
     {
       name: 'Justine',
-      techFocus: ['Bot Framework', 'Cognitive Services', 'Web Dev', 'Big Data'],
+      techFocus: ['Bot Framework', 'Cognitive Services', 'Big Data'],
       photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Justine.jpg'
     },
     {
-      name: 'Kim',
-      techFocus: ['Azure', 'SQL', 'C#'],
-      photoLink: ''
-    },
-    {
-      name: 'Robyn',
-      techFocus: ['Azure', 'SQL', 'C#'],
+      name: 'Santiago',
+      techFocus: ['Azure', 'Dev Ops'],
       photoLink: ''
     }
   ]
